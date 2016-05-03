06f9b883b91f607bee918794f8ac904199b00463
Jonas Weissensel <jweissensel@mailbox.org>
2016-05-03 17:53:21 +0200

    Short pwd path

--- __parse_current_folder.fish
+++ __parse_current_folder.fish
@@ -1,4 +1,6 @@
-#!/usr/bin/env fish
-function __parse_current_folder -d "Replace '$HOME' with '~'"
-  pwd | sed "s|$HOME|~|"
+function prompt_pwd --description 'Print the current working directory, shortened to fit the prompt'
+	set -l args_post
+	set -l args_pre
+	set -l realhome ~
+	echo $PWD | sed -e "s|^$realhome|~|" $args_pre -e 's-\([^/.]\)[^/]*/-\1/-g' $args_post
 end
